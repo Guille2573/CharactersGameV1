1ae17392c55311ab996d941a0fd86b466bf105cf
Guille2573 <gcarnf00@estudiantes.unileon.es>
2025-08-12 18:00:46 +0200

    cambios 5

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# No es necesario importar y parchear eventlet aquí, Gunicorn se encarga.
-# import eventlet
-# eventlet.monkey_patch()
+# app.py (Corregido y listo para producción)
 
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
@@ -8,8 +6,6 @@ from game import Game
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-
-# Dejamos async_mode='eventlet' para que Flask-SocketIO sepa qué esperar.
 socketio = SocketIO(app, async_mode='eventlet')
 
 game = Game(socketio)
@@ -17,8 +13,6 @@ game = Game(socketio)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# --- El resto de tus rutas y manejadores de eventos no cambian ---
 
 @socketio.on('join_game')
 def on_join(data):
@@ -43,8 +37,5 @@ def on_general_msg(data):
 def on_private_msg(data):
     game.handle_private_message(data)
 
-
 if __name__ == '__main__':
-    # Para pruebas locales, eventlet debe estar instalado
-    # y este comando usará el servidor de desarrollo de Flask
     socketio.run(app, debug=True)
